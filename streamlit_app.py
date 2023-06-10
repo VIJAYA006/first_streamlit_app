6c9fa931c79be1968febdcfbf0819bdc573c3718
VIJAYA006 <vijaya101992@gmail.com>
2023-06-10 15:50:44 +0530

    Update streamlit_app.py
    added emojis to menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ streamlit.title("My parents new healthy diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-boiled Free-Range Egg")
+streamlit.text(" 🥣 Omega 3 and Blueberry Oatmeal")
+streamlit.text(" 🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text(" 🐔  Hard-boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
