a332a0dc76438cf278edb5e4359d92ad86a26b75
VIJAYA006 <vijaya101992@gmail.com>
2023-06-10 21:49:14 +0530

    Update streamlit_app.py
    placed all imports on top

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+
 
 streamlit.title("My parents new healthy diner")
 streamlit.header("Breakfast Menu")
@@ -29,7 +33,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # prints the normalized json as table on screen
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
